7c56bbf22982b91fa3225c00c9971a6de8a6dd0f
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2024-02-26 12:35:59 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@ Route :: get('/projects', [ProjectController :: class, 'index'])
     -> name('project.index');
 Route :: get('/types', [TypeController :: class, 'index'])
     -> name('type.index');
+
+Route :: get('/projects/create', [ProjectController :: class, 'create'])
+    -> name('project.create');
+Route :: post('/projects/create', [ProjectController :: class, 'store'])
+    -> name('project.store');
+
+
+Route :: get('/projects/{id}/edit', [ProjectController :: class, 'edit'])
+    -> name('project.edit');
+Route :: put('/projects/{id}/edit', [ProjectController :: class, 'update'])
+    -> name('project.update');
